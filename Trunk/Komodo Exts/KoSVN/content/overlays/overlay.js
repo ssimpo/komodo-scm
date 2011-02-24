62dad4ad4bbc1a088e411adced9c14c56c69eec3
ss1770 <ss1770@localhost>
2011-02-24 10:25:35 +0000

    Added RepoBrowser Command

--- Trunk/Komodo Exts/KoSVN/content/overlays/overlay.js
+++ Trunk/Komodo Exts/KoSVN/content/overlays/overlay.js
@@ -3,7 +3,43 @@ if(!org) var com={};
 if(!org.simpo) org.simpo={};
 
 org.simpo.koSVN = function() {
-    hello: function() {
-        alert('Hello World');
-    }
+    //public object returned by this function
+    var pub = {};
+  
+    pub.repoBrowser = function() {
+        var project = getProject();
+        if (project) {
+            var path = getProjectPath(project);
+    
+            ko.run.runEncodedCommand(window, 'TortoiseProc.exe /command:repobrowser /path:\"'+path+'\" {\'cwd\': u\'%p\'}');
+        } else {
+            alert("Could not find current file in open projects.")
+        }
+    };
+    
+    pub._getProject = function() {
+        //  summary:
+        //      Get the project, the current file is attached to (will assume first
+        //      which it find the current file in).
+        //  returns: Object KomodoProject
+
+        try {
+            return ko.projects.manager.getCurrentProject();
+        } catch(e) {
+            return false;   
+        }
+    };
+    
+    pub._getProjectPath = function(project) {
+        // summary:
+        //      Get the path of a project.
+        // project: object KomodoProject
+        //      The project to get the path of.
+        // returns: string
+    
+        var projectFile = project.getFile();
+        return projectFile.dirName;
+    };
+    
+    return pub;
 }
