d40bf910817687a9f2b86069e25dd3cba53f79db
ss1770 <ss1770@localhost>
2011-05-13 11:36:32 +0000

    Move the function binding code into a separate file, so it isn't duplicated across content.

--- Trunk/Komodo Exts/SVNK/content/svnk.js
+++ Trunk/Komodo Exts/SVNK/content/svnk.js
@@ -11,24 +11,6 @@
 if (!org) var org = {};
 if (!org.simpo) org.simpo = {};
 
-// Current version of Komodo does not support Function.bind; this emulates
-// that functionality.
-if ( !Function.prototype.bind ) {
-    Function.prototype.bind = function( obj ) {
-        var slice = [].slice,
-        args = slice.call(arguments, 1),
-        self = this,
-        nop = function () {},
-        bound = function () {
-            return self.apply( this instanceof nop ? this : ( obj || {} ),
-                args.concat( slice.call(arguments) ) );
-        };
-        nop.prototype = self.prototype;
-        bound.prototype = new nop();
-        return bound;
-    };
-}
-
 try {
 
 org.simpo.svnk = function() {
