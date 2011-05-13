de6e835cc5b63ad6a7c3255db2c10f351955e516
ss1770 <ss1770@localhost>
2011-05-13 12:35:04 +0000

    Tagged version

---
+++ Tags/0.1.7/SVNK/content/dialogs/dialogs.js
@@ -0,0 +1,41 @@
+// summary:
+//      Main JavaScript content for SVN-K.
+// author:
+//      Stephen Simpson <me@simpo.org>
+// license:
+//      LGPL <http://www.gnu.org/licenses/lgpl.html>
+// version:
+//      0.1.7
+
+// Non violation of global namespace.
+if (!org) var org = {};
+if (!org.simpo) org.simpo = {};
+if (!org.simpo.svnk) org.simpo.svnk = {};
+
+try {
+    org.simpo.svnk.dialogs = function(arguments) {
+        
+        this.returner = arguments[0];
+        
+        this.save = function() {
+            this.returner.command = 'save';
+            window.close();
+	}
+	
+	this.cancel = function() {
+            this.returner.command = 'cancel';
+	    window.close();
+	}
+        
+        this.ignore = function() {
+            this.returner.command = 'ignore';
+	    window.close();
+	}
+        
+    }
+    
+    // Global namespace violation?
+    var SVNKDIALOG = new org.simpo.svnk.dialogs(window.arguments);
+} catch(e) {
+    Components.utils.reportError(e);    
+}
