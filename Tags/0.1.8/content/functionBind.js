5171f72cfb9cdd8e65c742035cd0f29078618052
ss1770 <ss1770@localhost>
2011-11-03 09:42:14 +0000

    Tagged-version copy

---
+++ Tags/0.1.8/content/functionBind.js
@@ -0,0 +1,26 @@
+// summary:
+//      Function binding code.
+// author:
+//      Stephen Simpson <me@simpo.org>
+// license:
+//      LGPL <http://www.gnu.org/licenses/lgpl.html>
+// version:
+//      0.1.9
+
+// Current version of Komodo does not support Function.bind; this emulates
+// that functionality.
+if ( !Function.prototype.bind ) {
+    Function.prototype.bind = function( obj ) {
+        var slice = [].slice,
+        args = slice.call(arguments, 1),
+        self = this,
+        nop = function () {},
+        bound = function () {
+            return self.apply( this instanceof nop ? this : ( obj || {} ),
+                args.concat( slice.call(arguments) ) );
+        };
+        nop.prototype = self.prototype;
+        bound.prototype = new nop();
+        return bound;
+    };
+}
