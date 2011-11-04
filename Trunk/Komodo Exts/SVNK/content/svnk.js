f1f4c6216b36503eecc3ae2127bf586bb3451603
ss1770 <ss1770@localhost>
2011-11-04 14:40:59 +0000

    * Added a check for updates feature * Started working on the drop-down menus for the toolbar buttons

--- Trunk/Komodo Exts/SVNK/content/svnk.js
+++ Trunk/Komodo Exts/SVNK/content/svnk.js
@@ -45,6 +45,13 @@ org.simpo.svnk = function() {
         
         this._runTortoiseCommand('repobrowser','project_then_directory','ErrorBrowserLoad');
     };
+    
+    this.repoStatus = function() {
+        // summary:
+        //      Open the repository browser for the current project.
+        
+        this._runTortoiseCommand('repostatus','project_then_directory','ErrorBrowserLoad');
+    };
 
     this.commit = function(type) {
         // summary:
@@ -73,7 +80,86 @@ org.simpo.svnk = function() {
         // summary:
         //      View the SVN log for item(s).
         
-        this._runTortoiseCommand('log',type,'ErrorViewLog');
+        /*var doc = this._getCurrentDocument(ko.windowManager.getMainWindow());
+        var menu = doc.getElementById('toolsToolbar-SVNK-ViewLogActiveFile-menupopup');
+        
+        var projects = this._getProjects();
+        for (i=0; i < projects.length; i++) {
+            var name = this._getProjectName(projects[i]);
+            var path = this._getProjectPath(projects[i]);
+            var item = doc.createElement('menuitem');
+            item.innerHTML = name;
+            item.value = path;
+            menu.appendChild(item);            
+        }*/
+        
+        //this._runTortoiseCommand('log',type,'ErrorViewLog');
+    };
+    
+    this.createMenu = function(menuNode,command) {
+        var md5Parser = new org.simpo.md5();
+        var lookup = {};
+        var doc = this._getCurrentDocument(ko.windowManager.getMainWindow());
+        var projects = this._getProjects();
+        
+        this._removeChildNodes(menuNode);
+        for (var i=0; i < projects.length; i++) {
+            var name = this._getProjectName(projects[i]);
+            var path = this._getProjectPath(projects[i]);
+            
+            var id = 'i' + md5Parser.calcMD5(path);
+            if (!(id in lookup)) {
+                lookup[id] = true;
+                var item = doc.createElement('menuitem');
+                item.setAttribute('label',name);
+                item.setAttribute('value',path);
+                menuNode.appendChild(item);
+            }
+        }
+        
+        var views = ko.views.manager.getAllViews();
+        for (var i = 0; i < views.length; i++) {
+            var view = views[i];
+            var file = this._getCurrentDocument(view).file;
+            var name = file.baseName;
+            var path = file.path;
+            
+            var id = 'i' + md5Parser.calcMD5(path);
+            if (!(id in lookup)) {
+                lookup[id] = true;
+                var item = doc.createElement('menuitem');
+                item.setAttribute('label',name);
+                item.setAttribute('value',path);
+                menuNode.appendChild(item);
+            }
+        }
+        
+        var selectedPaths = this._getSelectedPaths();
+        for (var i = 0; i < selectedPaths.length; i++) {
+            var name = selectedPaths[i];
+            var path = selectedPaths[i];
+            
+            var id = 'i' + md5Parser.calcMD5(path);
+            if (!(id in lookup)) {
+                lookup[id] = true;
+                var item = doc.createElement('menuitem');
+                item.setAttribute('label',name);
+                item.setAttribute('value',path);
+                menuNode.appendChild(item);
+            }
+        }
+        
+        
+    };
+    
+    this._removeChildNodes = function(node) {
+        if ((node === undefined) || (node === null)) {
+            return;
+        }
+        
+        while (node.hasChildNodes()) {
+            node.removeChild(node.firstChild());
+        }
     };
     
     this.viewProperties = function(type) {
@@ -146,6 +232,12 @@ org.simpo.svnk = function() {
                     path = this._getProjectPath();
                 } else {
                     path = this._getCurrentDirectoryPath();
+                }
+                break;
+            case 'activefile_then_project':
+                path = this._getCurrentFilePath();
+                if (!path) {
+                    path = this._getProjectPath();
                 }
                 break;
         }
@@ -333,6 +425,33 @@ org.simpo.svnk = function() {
             );
             return false; 
         }
+    };
+    
+    this._getProjects = function() {
+        var projectsTree = '';
+        if (ko.places.projects_SPV) {
+            projectsTree = ko.places.projects_SPV.projectsTree.view;
+        } else {
+            projectsTree = ko.places.projects.projectsTree.view;
+        }
+        
+        var projects = new Array();
+        for (i=0; i < projectsTree.rowCount; i++) {
+            projects.push(projectsTree.getRowItem(i).project);
+        }
+        
+        return projects;
+    };
+    
+    this._getProjectName = function(project) {
+        if (project == undefined) {
+            project = this._getProject();
+        }
+        
+        var name = project.name.replace('.komodoproject','');
+        name = name.replace('.kpf','');
+        
+        return name;
     };
     
     this._getProjectPath = function(project) {
@@ -604,6 +723,152 @@ org.simpo.svnk = function() {
         } catch(e) {
             return {error:true,value:e};
         }
+    };
+};
+
+org.simpo.md5 = function() {
+/*
+ * A JavaScript implementation of the RSA Data Security, Inc. MD5 Message
+ * Digest Algorithm, as defined in RFC 1321.
+ */
+
+    this.hex_chr = '0123456789abcdef';
+    
+    this.rhex = function(num) {
+        var str = "";
+        for (var j = 0; j <= 3; j++)
+        str += this.hex_chr.charAt((num >> (j * 8 + 4)) & 0x0F) + this.hex_chr.charAt((num >> (j * 8)) & 0x0F);
+        return str;
+    };
+
+    this.str2blks_MD5 = function(str) {
+        var nblk = ((str.length + 8) >> 6) + 1;
+        var blks = new Array(nblk * 16);
+        for (var i = 0; i < nblk * 16; i++) blks[i] = 0;
+        for (var i = 0; i < str.length; i++)
+        blks[i >> 2] |= str.charCodeAt(i) << ((i % 4) * 8);
+        blks[i >> 2] |= 0x80 << ((i % 4) * 8);
+        blks[nblk * 16 - 2] = str.length * 8;
+        return blks;
+    };
+    
+    this.add = function(x, y) {
+        var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+        var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+        return (msw << 16) | (lsw & 0xFFFF);
+    };
+ 
+    this.rol = function(num, cnt) {
+        return (num << cnt) | (num >>> (32 - cnt));
+    };
+    
+    this.cmn = function(q, a, b, x, s, t) {
+        return this.add(this.rol(this.add(this.add(a, q), this.add(x, t)), s), b);
+    };
+    
+    this.ff = function(a, b, c, d, x, s, t) {
+        return this.cmn((b & c) | ((~b) & d), a, b, x, s, t);
+    };
+    
+    this.gg = function(a, b, c, d, x, s, t) {
+        return this.cmn((b & d) | (c & (~d)), a, b, x, s, t);
+    };
+    
+    this.hh = function(a, b, c, d, x, s, t) {
+        return this.cmn(b ^ c ^ d, a, b, x, s, t);
+    };
+    
+    this.ii = function(a, b, c, d, x, s, t) {
+        return this.cmn(c ^ (b | (~d)), a, b, x, s, t);
+    };
+
+    this.calcMD5 = function(str) {
+        var x = this.str2blks_MD5(str);
+        var a = 1732584193;
+        var b = -271733879;
+        var c = -1732584194;
+        var d = 271733878;
+        
+        for (var i = 0; i < x.length; i += 16) {
+            var olda = a;
+            var oldb = b;
+            var oldc = c;
+            var oldd = d;
+            
+            a = this.ff(a, b, c, d, x[i+ 0], 7 , -680876936);
+            d = this.ff(d, a, b, c, x[i+ 1], 12, -389564586);
+            c = this.ff(c, d, a, b, x[i+ 2], 17,  606105819);
+            b = this.ff(b, c, d, a, x[i+ 3], 22, -1044525330);
+            a = this.ff(a, b, c, d, x[i+ 4], 7 , -176418897);
+            d = this.ff(d, a, b, c, x[i+ 5], 12,  1200080426);
+            c = this.ff(c, d, a, b, x[i+ 6], 17, -1473231341);
+            b = this.ff(b, c, d, a, x[i+ 7], 22, -45705983);
+            a = this.ff(a, b, c, d, x[i+ 8], 7 ,  1770035416);
+            d = this.ff(d, a, b, c, x[i+ 9], 12, -1958414417);
+            c = this.ff(c, d, a, b, x[i+10], 17, -42063);
+            b = this.ff(b, c, d, a, x[i+11], 22, -1990404162);
+            a = this.ff(a, b, c, d, x[i+12], 7 ,  1804603682);
+            d = this.ff(d, a, b, c, x[i+13], 12, -40341101);
+            c = this.ff(c, d, a, b, x[i+14], 17, -1502002290);
+            b = this.ff(b, c, d, a, x[i+15], 22,  1236535329);
+            
+            a = this.gg(a, b, c, d, x[i+ 1], 5 , -165796510);
+            d = this.gg(d, a, b, c, x[i+ 6], 9 , -1069501632);
+            c = this.gg(c, d, a, b, x[i+11], 14,  643717713);
+            b = this.gg(b, c, d, a, x[i+ 0], 20, -373897302);
+            a = this.gg(a, b, c, d, x[i+ 5], 5 , -701558691);
+            d = this.gg(d, a, b, c, x[i+10], 9 ,  38016083);
+            c = this.gg(c, d, a, b, x[i+15], 14, -660478335);
+            b = this.gg(b, c, d, a, x[i+ 4], 20, -405537848);
+            a = this.gg(a, b, c, d, x[i+ 9], 5 ,  568446438);
+            d = this.gg(d, a, b, c, x[i+14], 9 , -1019803690);
+            c = this.gg(c, d, a, b, x[i+ 3], 14, -187363961);
+            b = this.gg(b, c, d, a, x[i+ 8], 20,  1163531501);
+            a = this.gg(a, b, c, d, x[i+13], 5 , -1444681467);
+            d = this.gg(d, a, b, c, x[i+ 2], 9 , -51403784);
+            c = this.gg(c, d, a, b, x[i+ 7], 14,  1735328473);
+            b = this.gg(b, c, d, a, x[i+12], 20, -1926607734);
+            
+            a = this.hh(a, b, c, d, x[i+ 5], 4 , -378558);
+            d = this.hh(d, a, b, c, x[i+ 8], 11, -2022574463);
+            c = this.hh(c, d, a, b, x[i+11], 16,  1839030562);
+            b = this.hh(b, c, d, a, x[i+14], 23, -35309556);
+            a = this.hh(a, b, c, d, x[i+ 1], 4 , -1530992060);
+            d = this.hh(d, a, b, c, x[i+ 4], 11,  1272893353);
+            c = this.hh(c, d, a, b, x[i+ 7], 16, -155497632);
+            b = this.hh(b, c, d, a, x[i+10], 23, -1094730640);
+            a = this.hh(a, b, c, d, x[i+13], 4 ,  681279174);
+            d = this.hh(d, a, b, c, x[i+ 0], 11, -358537222);
+            c = this.hh(c, d, a, b, x[i+ 3], 16, -722521979);
+            b = this.hh(b, c, d, a, x[i+ 6], 23,  76029189);
+            a = this.hh(a, b, c, d, x[i+ 9], 4 , -640364487);
+            d = this.hh(d, a, b, c, x[i+12], 11, -421815835);
+            c = this.hh(c, d, a, b, x[i+15], 16,  530742520);
+            b = this.hh(b, c, d, a, x[i+ 2], 23, -995338651);
+            
+            a = this.ii(a, b, c, d, x[i+ 0], 6 , -198630844);
+            d = this.ii(d, a, b, c, x[i+ 7], 10,  1126891415);
+            c = this.ii(c, d, a, b, x[i+14], 15, -1416354905);
+            b = this.ii(b, c, d, a, x[i+ 5], 21, -57434055);
+            a = this.ii(a, b, c, d, x[i+12], 6 ,  1700485571);
+            d = this.ii(d, a, b, c, x[i+ 3], 10, -1894986606);
+            c = this.ii(c, d, a, b, x[i+10], 15, -1051523);
+            b = this.ii(b, c, d, a, x[i+ 1], 21, -2054922799);
+            a = this.ii(a, b, c, d, x[i+ 8], 6 ,  1873313359);
+            d = this.ii(d, a, b, c, x[i+15], 10, -30611744);
+            c = this.ii(c, d, a, b, x[i+ 6], 15, -1560198380);
+            b = this.ii(b, c, d, a, x[i+13], 21,  1309151649);
+            a = this.ii(a, b, c, d, x[i+ 4], 6 , -145523070);
+            d = this.ii(d, a, b, c, x[i+11], 10, -1120210379);
+            c = this.ii(c, d, a, b, x[i+ 2], 15,  718787259);
+            b = this.ii(b, c, d, a, x[i+ 9], 21, -343485551);
+            
+            a = this.add(a, olda);
+            b = this.add(b, oldb);
+            c = this.add(c, oldc);
+            d = this.add(d, oldd);
+        }
+        return this.rhex(a) + this.rhex(b) + this.rhex(c) + this.rhex(d);
     };
 };
 
