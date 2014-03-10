97e1903ff04226a0e69aba14179df6e3a65ea76b
Stephen Simpson <me@simpo.org>
2014-03-10 17:03:29 +0000

    Added templated parametres from the manifest for insertion at build time.

--- app/content/functionBind.js
+++ app/content/functionBind.js
@@ -1,11 +1,11 @@
 // summary:
 //      Function binding code.
 // author:
-//      Stephen Simpson <me@simpo.org>
+//      <%= creator %> <me@simpo.org>
 // license:
 //      LGPL <http://www.gnu.org/licenses/lgpl.html>
 // version:
-//      0.2.1b
+//      <%= version %>
 
 // Current version of Komodo does not support Function.bind; this emulates
 // that functionality.
--- app/content/prefEdit.js
+++ app/content/prefEdit.js
@@ -1,11 +1,11 @@
 // summary:
 //      Set/Get preference settings.
 // author:
-//      Stephen Simpson <me@simpo.org>
+//      <%= creator %> <me@simpo.org>
 // license:
 //      LGPL <http://www.gnu.org/licenses/lgpl.html>
 // version:
-//      0.2.1b
+//      <%= version %>
 
 // Non violation of global namespace.
 if (!org) var org = {};
