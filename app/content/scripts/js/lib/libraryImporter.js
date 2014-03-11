96fa3335e24a823dd1bd5304f5a2664583d67d78
ssimpo <ssimpo@gmail.com>
2014-03-11 15:00:56 +0000

    Refactored the name 'library' to 'package' as this better represents the meaning.

--- app/content/scripts/js/lib/libraryImporter.js
+++ app/content/scripts/js/lib/libraryImporter.js
@@ -1,8 +1,8 @@
 var EXPORTED_SYMBOLS = ["require"];
 
 var _this = this;
-var _loadedLibraries = {};
-var _libraryPaths = {};
+var _loadedPackages = {};
+var _packagePaths = {};
 var _cacheBust = false;
 
 var require = function(config, moduleIds, callback){
@@ -18,7 +18,7 @@ var require = function(config, moduleIds, callback){
 	//		The function to return the modules into.
 	
 	if(moduleIds === undefined){
-		_loadLibraryPaths(config, false);
+		_loadPackagePaths(config, false);
 		if(_isProperty(config, "cacheBust")){
 			_cacheBust = config.cacheBust;
 		}
@@ -50,7 +50,7 @@ function _require(config, moduleIds, callback){
 	//	callback: function
 	//		The function to return the modules into.
 	
-	var paths = _loadLibraryPaths(config);
+	var paths = _loadPackagePaths(config);
 	var cacheBust = ((_isProperty(config, "cacheBust"))
 		? config.cacheBust
 		: _cacheBust
@@ -69,12 +69,12 @@ function _require(config, moduleIds, callback){
 	callback.apply(_this, params);
 }
 
-function _loadLibraryPaths(config, useTemp){
+function _loadPackagePaths(config, useTemp){
 	// summary:
-	//		Load the library paths from a config object.
+	//		Load the package paths from a config object.
 	// description:
-	//		Load the library paths from a given config object. A temparary
-	//		library path mapping object is created and returned.  Depending on
+	//		Load the package paths from a given config object. A temparary
+	//		package path mapping object is created and returned.  Depending on
 	//		on the value of useTemp (default: true), the global paths are
 	//		also overriden so it affects future operations.
 	// config: object
@@ -86,16 +86,16 @@ function _loadLibraryPaths(config, useTemp){
 	//			{"<package id>":"<package root directory path (absolute)>"}.
 	
 	useTemp = ((useTemp === undefined) ? true : useTemp);
-	var tempLibraryPaths = cloneObject(_libraryPaths);
+	var tempPackagePaths = cloneObject(_packagePaths);
 	
 	config.packages.forEach(function(packageDetails){
-		tempLibraryPaths[packageDetails.name] = packageDetails.location;
+		tempPackagePaths[packageDetails.name] = packageDetails.location;
 		if(!useTemp){
-			_libraryPaths[packageDetails.name] = packageDetails.location;
+			_packagePaths[packageDetails.name] = packageDetails.location;
 		}
 	});
 	
-	return tempLibraryPaths;
+	return tempPackagePaths;
 }
 
 function _resolveModuleId(moduleId, paths, cacheBust){
@@ -105,12 +105,12 @@ function _resolveModuleId(moduleId, paths, cacheBust){
 	//		Calculate and return the full path to a given Javascript resource
 	//		based on the supplied module-id and the current config settings.
 	// moduleId: string
-	//		Module-id in the format aa/bb/... Where is aa is the base library
+	//		Module-id in the format aa/bb/... Where is aa is the base package
 	//		and should have been previously listed in the config.packages
 	//		settings.  bb and anything after it the directory path from aa
-	//		library root.
+	//		package root.
 	// paths: object
-	//		Object containing the current library paths (packagages) to be
+	//		Object containing the current package paths (packagages) to be
 	//		used for this resolve.
 	// cacheBust: boolean|mixed
 	//		The cacheBust setting for this resolve operation.
@@ -120,13 +120,13 @@ function _resolveModuleId(moduleId, paths, cacheBust){
 	cacheBust = ((cacheBust === undefined) ? _cacheBust : cacheBust);
 	
 	var idParts = moduleId.split("/");
-	var library = idParts.shift();
+	var cPackage = idParts.shift();
 	var filePath =
 		"/" + idParts.join("/") + ".js?cachBust="
 		+ _getCacheVarForModule(moduleId, cacheBust);
 	
-	if(_isProperty(paths, library)){
-		filePath = paths[library] + filePath;
+	if(_isProperty(paths, cPackage)){
+		filePath = paths[cPackage] + filePath;
 	}else{
 		var error = new Error("Could not load module: " + moduleId);
 		Components.utils.reportError(error);
@@ -138,9 +138,9 @@ function _resolveModuleId(moduleId, paths, cacheBust){
 
 function _getCacheVarForModule(moduleName, cacheBust){
 	// summary:
-	//		Get a cache query paramater for the given library.
+	//		Get a cache query paramater for the given package.
 	// description:
-	//		Return a unique interger id for the given library name. The id will
+	//		Return a unique interger id for the given package name. The id will
 	//		calculated depeding on the value of cacheBust. The id will be used
 	//		to control the caching of module loads.
 	//
@@ -149,7 +149,7 @@ function _getCacheVarForModule(moduleName, cacheBust){
 	//		module already has a cache id.  If it is a value then use the value
 	//		as the cache id.
 	// moduleName: string
-	//		The name of the library.
+	//		The name of the package.
 	// cacheBust: boolean|mixed
 	//		Generate a new cache id for each module for each load: True or
 	//		False.  If not a boolean then use the value as a cache id or use
@@ -161,11 +161,11 @@ function _getCacheVarForModule(moduleName, cacheBust){
 	cacheBust = ((cacheBust === undefined) ? _cacheBust : cacheBust);
 	
 	if(cacheBust === false){
-		if(!_isProperty(_loadedLibraries, moduleName)){
-			_loadedLibraries[moduleName] = _getRandomId();
+		if(!_isProperty(_loadedPackages, moduleName)){
+			_loadedPackages[moduleName] = _getRandomId();
 		}
 		
-		return _loadedLibraries[moduleName];
+		return _loadedPackages[moduleName];
 	}else if(cacheBust === true){
 		return _getRandomId();
 	}else{
