1f3d36b49ea05ba96e5330ebd1ac743de218effb
ssimpo <ssimpo@gmail.com>
2014-03-21 13:57:07 +0000

    Updates to help Unit Testing

---
+++ test/unit/bin/lib/fs2.js
@@ -0,0 +1,8 @@
+var fs = require('../../../../bin/scripts/lib/fs2.js');
+
+describe("Various file functions", function(){
+	it("Initial filter setup", function(){
+		var filters = fs._initFilters();
+	});
+});
+
